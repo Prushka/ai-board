bf056c7d33491e9d49809dc8c199f325d04bc5b5
Prushka <dan@lyu.sh>
2026-02-03 15:31:46 +0800

    Add overflow behavior none and pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // @ts-expect-error - Silence Turbopack warning as we use next-pwa (webpack plugin)
+  turbopack: {},
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
